16c94c5d67179b595e30987b7b3fafebe087905b
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-24 00:06:04 +0000

    kill it now

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # kills a process named killmenow
 exec { "kill killmenow":
-  command  => 'pkill killmenow',
-  provider => shell,
+  command  => 'pkill -f killmenow',
+  path => '/usr/bin:/bin',
 }
