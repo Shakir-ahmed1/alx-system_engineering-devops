f1cf50c53da8ed5b0ac78188c443f0851da0c4b7
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-29 19:21:14 +0000

    replacing content with command

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # server side ssh config
 
 exec { 'config':
-  content => "echo 'Host server HostName 54.87.151.225\n\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no' >> /etc/ssh/sshd_config",
+  command => "echo 'Host server HostName 54.87.151.225\n\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no' >> /etc/ssh/sshd_config",
   path    => '/usr/bin',
 }
 
