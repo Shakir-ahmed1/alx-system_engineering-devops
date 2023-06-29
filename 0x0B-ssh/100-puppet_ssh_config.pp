c7a69f58d053caadfd44914e79b23075ec49ed6d
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-29 19:22:20 +0000

    fixing verbosity

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # server side ssh config
 
 exec { 'config':
-  command => "echo 'Host server HostName 54.87.151.225\n\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no' >> /etc/ssh/sshd_config",
+  command => "echo 'Host server\n\tHostName 54.87.151.225\n\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no' >> /etc/ssh/sshd_config",
   path    => '/usr/bin',
 }
 
