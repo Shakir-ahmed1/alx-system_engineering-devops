770a90435003e4cb4493083d0c5efd39bd83f6af
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-29 19:03:16 +0000

    changing the config from client to server

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,6 +2,6 @@
 
 file { 'config':
   path    => '/etc/ssh/sshd_config',
-  content => 'Host server1\\n\\tHostName 54.87.151.225\\n\\tUser ubuntu\\n\\tIdentityFile ~/.ssh/school\\n\\tPasswordAuthentication no'
+  content => 'IdentityFile ~/.ssh/school\\nPasswordAuthentication no'
 }
 
