d9e3a9cf34a3a093bd625094540a2190b99ebc04
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-10-10 21:30:27 +0000

    get data while recursing

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+module that returns a list containing the titles of all hot articles for a
+given subreddit
+"""
+import requests
+
+def recurse(subreddit, params={}, hot_list=[]):
+    """ returns a list of titles """
+    if params is not None:
+        link = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        response = requests.get(link, headers=headers, params=params)
+        r = response.json()
+        if not r or 'error' in r:
+            return None
+        
+        for post in r['data']['children']:
+            hot_list.append(post['data']['title'])
+        
+        params = {'after':r['data']['after']}
+        if params['after']:
+            return recurse(subreddit, params=params, hot_list=hot_list)
+    return list(set(hot_list))
