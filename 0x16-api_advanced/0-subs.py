5cf6c204279d0666cfa46b7ad979294b722ad788
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-10-10 19:43:53 +0000

    pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """ returns number of subscribers of a subreddit """
     headers = {'User-Agent': 'Mozilla/5.0'}
-    link = 'https://www.reddit.com/r/{}/about.json'.format(subreddit) 
+    link = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = requests.get(link, headers=headers)
     r = response.json()
     if not r or 'error' in r:
